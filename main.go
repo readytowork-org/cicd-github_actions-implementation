b085c66c5412146a5d87086701f2499d39f5062e
Sumit <sumitsapkota0@gmail.com>
2021-11-15 12:29:20 +0545

    removed docker container for now

--- main.go
+++ main.go
@@ -3,22 +3,32 @@ package main
 import (
 	"io"
 	"log"
+	"net"
 	"net/http"
-	"os"
 )
 
 func main() {
 	http.HandleFunc("/health-check", helloHandler)
-
-	log.Println("Listning for requests at /health-check")
-	port, found := os.LookupEnv("PORT")
-	if !found {
-		port = "8000"
-	}
-	log.Fatal(http.ListenAndServe(port, nil))
+	http.HandleFunc("/", GetLocalIP)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func helloHandler(w http.ResponseWriter, req *http.Request) {
-
 	io.WriteString(w, "Tada, 👻 Server is up and running!\n")
+}
+
+// GetLocalIP returns the non loopback local IP of the host
+func GetLocalIP(w http.ResponseWriter, req *http.Request) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				io.WriteString(w, "Your IP address is\n")
+			}
+		}
+	}
 }
